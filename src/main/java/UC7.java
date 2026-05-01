fbb14435aadb1223f6426cb5cbe3303432f3cd73
shreyascode11 <shreoriginal@gmail.com>
2026-05-01 22:03:32 +0530

    UC8 Completed

--- src/main/java/UC7.java
+++ src/main/java/UC7.java
@@ -1,6 +1,4 @@
-import java.util.Random;
-
-public class UC7 {
+public class UC8 {
 
     static char[][] board = {
             {'-', '-', '-'},
@@ -8,25 +6,74 @@ public class UC7 {
             {'-', '-', '-'}
     };
 
-    static char computerSymbol = 'O';
+    static boolean isHumanTurn = true;
+    static boolean gameOver = false;
 
     public static void main(String[] args) {
-        computerMove();
-        printBoard();
+
+        while (!gameOver) {
+
+            if (isHumanTurn) {
+                humanMove();
+            } else {
+                computerMove();
+            }
+
+            printBoard();
+
+            if (hasWon('X') || hasWon('O') || isBoardFull()) {
+                gameOver = true;
+            } else {
+                isHumanTurn = !isHumanTurn;
+            }
+        }
+    }
+
+    static void humanMove() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    board[i][j] = 'X';
+                    return;
+                }
+            }
+        }
     }
 
     static void computerMove() {
-        Random rand = new Random();
-
-        while (true) {
-            int row = rand.nextInt(3);
-            int col = rand.nextInt(3);
-
-            if (board[row][col] == '-') {
-                board[row][col] = computerSymbol;
-                break;
+        for (int i = 2; i >= 0; i--) {
+            for (int j = 2; j >= 0; j--) {
+                if (board[i][j] == '-') {
+                    board[i][j] = 'O';
+                    return;
+                }
             }
         }
+    }
+
+    static boolean hasWon(char symbol) {
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) return true;
+        }
+
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] == symbol && board[1][j] == symbol && board[2][j] == symbol) return true;
+        }
+
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) return true;
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) return true;
+
+        return false;
+    }
+
+    static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') return false;
+            }
+        }
+        return true;
     }
 
     static void printBoard() {
@@ -36,5 +83,6 @@ public class UC7 {
             }
             System.out.println();
         }
+        System.out.println();
     }
 }
