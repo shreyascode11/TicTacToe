42108cc2892cdc0432600d85b9e05fed615dce37
shreyascode11 <shreoriginal@gmail.com>
2026-05-08 01:42:31 +0530

    UC10  Completed

---
+++ src/main/java/UC10.java
@@ -0,0 +1,23 @@
+public class UC10 {
+
+    static char[][] board = {
+            {'X', 'O', 'X'},
+            {'O', 'X', 'O'},
+            {'O', 'X', 'O'}
+    };
+
+    public static void main(String[] args) {
+        System.out.println(isDraw());
+    }
+
+    static boolean isDraw() {
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (board[r][c] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
